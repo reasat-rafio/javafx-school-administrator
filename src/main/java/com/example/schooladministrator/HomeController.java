b1bfb39254445daf47316edd0c59bd1482b57e85
reasat-rafio <rafioar96@gmail.com>
2022-04-09 17:35:16 +0600

    :lipstick: add student scene ui added

--- src/main/java/com/example/schooladministrator/HomeController.java
+++ src/main/java/com/example/schooladministrator/HomeController.java
@@ -14,13 +14,12 @@ import java.util.Objects;
 public class HomeController {
     @FXML
     private Stage stage;
-    private Scene scene;
 
 
     public void sceneSwitcher(String path, ActionEvent e) throws IOException {
         Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource(path)));
         stage = (Stage)((Node)e.getSource()).getScene().getWindow();
-        scene = new Scene(root);
+        Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
     }
