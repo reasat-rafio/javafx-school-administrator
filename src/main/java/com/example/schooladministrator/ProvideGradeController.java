4fdc98c111a575c5fd662f2a6ded469f3a9725e8
reasat-rafio <rafioar96@gmail.com>
2022-04-24 11:04:17 +0600

    :zap: insert action added

--- src/main/java/com/example/schooladministrator/ProvideGradeController.java
+++ src/main/java/com/example/schooladministrator/ProvideGradeController.java
@@ -7,6 +7,8 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
@@ -28,10 +30,11 @@ public class ProvideGradeController implements Initializable {
     private TextField finalNumber;
     @FXML
     private ComboBox<String> subjectSelector;
+    @FXML
+    private Button insertBtn;
 
     Student student;
     ArrayList<Subject> result = new ArrayList<Subject>();
-
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -44,6 +47,7 @@ public class ProvideGradeController implements Initializable {
         name.setText(student.getFirstName() + " " + student.getLastName());
         ID.setText(student.getStudentId());
         subjectSelector.getSelectionModel().selectFirst();
+        insertBtn.setText("Insert " + subjectSelector.getSelectionModel().getSelectedItem() + " Numbers");
     }
 
     public void selectSubjectAction(ActionEvent event) {
@@ -60,11 +64,11 @@ public class ProvideGradeController implements Initializable {
                 }
             if(updateIndex >= 0){
                 System.out.println(result.get(updateIndex));
-                midNumber.setText(String.valueOf(result.get(updateIndex).getMidNumber()));
-                finalNumber.setText(String.valueOf(result.get(updateIndex).getFinalNumber()));
+//                midNumber.setText(String.valueOf(result.get(updateIndex).getMidNumber()));
+//                finalNumber.setText(String.valueOf(result.get(updateIndex).getFinalNumber()));
             } else {
-                midNumber.setText("");
-                finalNumber.setText("");
+//                midNumber.setText("");
+//                finalNumber.setText("");
             }
         }
 
@@ -72,8 +76,7 @@ public class ProvideGradeController implements Initializable {
         System.out.println(subjectSelector.getSelectionModel().getSelectedItem());
     }
 
-    public void onSubmitAction(ActionEvent e) {
-
+    public void onInsertAction(ActionEvent event) {
         String selectedSubject = subjectSelector.getSelectionModel().getSelectedItem();
         int midNum= Integer.parseInt(midNumber.getText());
         int finalNum = Integer.parseInt(finalNumber.getText());
@@ -90,15 +93,27 @@ public class ProvideGradeController implements Initializable {
                 }
             }
             if(updateIndex >= 0) {
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setContentText(selectedSubject + "'s number updated");
+                alert.showAndWait();
                 result.get(updateIndex).setMidNumber(midNum);
                 result.get(updateIndex).setFinalNumber(finalNum);
             } else {
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setContentText(selectedSubject + "'s number added");
+                alert.showAndWait();
                 result.add(new Subject(selectedSubject, midNum, finalNum));
             }
         } else {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setContentText(selectedSubject + "'s number added");
+            alert.showAndWait();
             result.add(new Subject(selectedSubject, midNum, finalNum));
         }
 
+    }
+
+    public void onSubmitAction(ActionEvent e) {
         for(Subject s:result){
             System.out.println(s.toString());
         }
